8b81b02d2af244863a4e4c5d19b5bd425bf2e9c0
Carlos Eduardo dos Santos Vieira <kiltz@Carloss-MacBook-Air.local>
2024-09-16 21:12:37 -0300

    update

--- main.js
+++ main.js
@@ -1,26 +1,29 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const express = require("express");
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const app = express();
 
-
 const connection = mysql.createConnection({
-    host: process.env.host,
-    user: process.env.user,
-    password: process.env.password,
-    database: process.env.database
+  host: process.env.host,
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database,
 });
 
 connection.connect((err) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log("Connected to the database");
-        console.log("Sucesso, parabens!");
-    }
-})
+  if (err) {
+    console.log(err);
+  } else {
+    connection.query("SELECT * FROM cliente", (err, results) => {
+        if(err){
+            console.log(err);
+        }
+      console.log(results);
+    });
+  }
+});
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-})
+  console.log("Server is running on port 3000");
+});
